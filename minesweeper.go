b359a75a9c61f742ccc6d2fa163837668fe30806
Daniel Sedlak <daniel@sedlak.biz>
2018-08-12 19:02:40 +0200

    Finishing game logic, refactor code

--- minesweeper.go
+++ minesweeper.go
@@ -1,15 +1,17 @@
 package minesweeper
 
 import (
+	"errors"
 	"math/rand"
 	"time"
 )
 
 var (
-	gameField  []Tile
 	isGameOver bool
+	gameField  GameField
 )
 
+// Tile for GameField
 type Tile struct {
 	X      uint
 	Y      uint
@@ -18,12 +20,15 @@ type Tile struct {
 	Around uint
 }
 
-func GenerateGame(xSize, ySize, numberOfMines uint) []Tile {
-	gameField := []Tile{}
+// GameField is type of game
+type GameField []Tile
+
+// GenerateGame creates new GameField
+func GenerateGame(xSize, ySize, numberOfMines uint) (GameField, error) {
 	minesLeft := numberOfMines
 
-	if xSize*ySize > numberOfMines {
-		// ERROR
+	if xSize*ySize < numberOfMines {
+		return nil, errors.New("Too many mines for game")
 	}
 
 	for x := uint(0); x < xSize; x++ {
@@ -31,25 +36,24 @@ func GenerateGame(xSize, ySize, numberOfMines uint) []Tile {
 			t := "normal"
 			if minesLeft != 0 {
 				t = "mine"
+				minesLeft--
 			}
 			gameField = append(gameField, Tile{X: x, Y: y, Type: t, Hidden: true, Around: 0})
 		}
 	}
 
-	return gameField
+	gameField.shuffle()
+	gameField.checkForCorrectIndex(xSize, ySize)
+	gameField.calculateAround()
+
+	// Make deep copy
+	output := make(GameField, len(gameField))
+	copy(output, gameField)
+	return GameField(output), nil
 }
 
-func GetTile(x, y uint) Tile {
-	for _, item := range gameField {
-		if item.X == x && item.Y == y {
-			return item
-		}
-	}
-	// ERROR
-	return Tile{}
-}
-
-func RevealItem(x, y uint) Tile {
+// RevealTile Reveals tile in game
+func RevealTile(x, y uint) (Tile, error) {
 	for _, i := range gameField {
 		if i.X == x && i.Y == y {
 			if i.Type == "mine" {
@@ -57,27 +61,74 @@ func RevealItem(x, y uint) Tile {
 			} else {
 				isGameOver = false
 			}
-			return i
+			return i, nil
 		}
 	}
 
 	// ERROR
-	return Tile{}
+	return Tile{}, errors.New("Not found")
 }
 
-func GetBoard() []Tile {
-	return gameField
-}
-
-func GetGameStatus() bool {
-	return isGameOver
-}
-
-func generateMines() {
+func (gf GameField) shuffle() {
 	source := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(source)
 
-	for {
+	for i := range gf {
+		num := r.Intn(len(gf) - 1)
 
+		gf[i], gf[num] = gf[num], gf[i]
 	}
+}
+
+func (gf GameField) checkForCorrectIndex(xSize uint, ySize uint) {
+	x := uint(0)
+	y := uint(0)
+
+	for i := range gf {
+		gf[i].X = x
+		gf[i].Y = y
+		if y == ySize {
+			y = 0
+			x++
+		} else {
+			y++
+		}
+	}
+}
+
+func (gf GameField) calculateAround() {
+	for i := range gf {
+		var carry uint
+		element := gf[i]
+
+		tilesAround := make([]Tile, 8)
+
+		tilesAround[0], _ = gf.check(element.X-1, element.Y-1)
+		tilesAround[1], _ = gf.check(element.X-1, element.Y)
+		tilesAround[2], _ = gf.check(element.X-1, element.Y+1)
+		tilesAround[3], _ = gf.check(element.X, element.Y-1)
+		tilesAround[4], _ = gf.check(element.X, element.Y+1)
+		tilesAround[5], _ = gf.check(element.X+1, element.Y-1)
+		tilesAround[6], _ = gf.check(element.X+1, element.Y)
+		tilesAround[7], _ = gf.check(element.X+1, element.Y+1)
+
+		for _, item := range tilesAround {
+			if item.Type == "mine" {
+				carry++
+			}
+		}
+
+		gameField[i].Around = carry
+	}
+}
+
+func (gf GameField) check(x, y uint) (Tile, error) {
+	for _, item := range gameField {
+		if item.X == x && item.Y == y {
+			return item, nil
+		}
+	}
+
+	// ERROR
+	return Tile{}, errors.New("Not found")
 }
--- minesweeper_test.go
+++ minesweeper_test.go
@@ -1,1 +1,19 @@
 package minesweeper
+
+import (
+	"testing"
+)
+
+func TestGenerateGame(t *testing.T) {
+	var x uint = 5
+	var y uint = 5
+	board, err := GenerateGame(x, y, 5)
+
+	if err == nil {
+		if uint(len(board)) != x*y {
+			t.Errorf("Wront array length. Have %v expected %v", len(board), x*y)
+		}
+	} else {
+		t.Error(err)
+	}
+}
